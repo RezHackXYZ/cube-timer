66f48ab7f861812be91f628ed048efb8fb1c6837
RezHackXYZ <rhythmupa@gmail.com>
2025-06-19 01:30:04 +0530

    make it slightly good!

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,43 +1,43 @@
 <script>
-	let minutes = 90;
-	let seconds = 0;
-	let milliseconds = 0;
+	let minutes = $state(0);
+	let seconds = $state(0);
+	let milliseconds = $state(0);
 
-	let updateTime;
+	let updateTime = $state();
 </script>
 
-<span class="font-mono">
-	{minutes.toString().padStart(2, '0')}:
-	{seconds.toString().padStart(2, '0')}.
-	{milliseconds.toString().padStart(2, '0')}
-</span>
+<div class="flex h-full flex-col items-center justify-center gap-4">
+	<span class="font-mono text-5xl">
+		{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}.{milliseconds
+			.toString()
+			.padStart(2, '0')}
+	</span>
 
-<br />
+	<div>
+		<button
+			class="btn {updateTime ? 'red' : 'green'}"
+			onclick={() => {
+				if (updateTime) {
+					clearInterval(updateTime);
+					updateTime = null;
+				} else {
+					updateTime = setInterval(async () => {
+						milliseconds++;
 
-<button
-	onclick={() => {
-		updateTime = setInterval(async () => {
-			milliseconds++;
+						if (milliseconds >= 100) {
+							milliseconds = 0;
+							seconds++;
 
-			if (milliseconds >= 100) {
-				milliseconds = 0;
-				seconds++;
-
-				if (seconds >= 60) {
-					seconds = 0;
-					minutes++;
+							if (seconds >= 60) {
+								seconds = 0;
+								minutes++;
+							}
+						}
+					}, 10);
 				}
-			}
-		}, 10);
-	}}
->
-	start
-</button>
-
-<button
-	onclick={() => {
-		clearInterval(updateTime);
-	}}
->
-	stop
-</button>
+			}}
+		>
+			{updateTime ? 'stop' : 'start'}
+		</button>
+	</div>
+</div>
