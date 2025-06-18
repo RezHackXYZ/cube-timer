e27fbc11b7197ba8c29d1398ebef560793173961
RezHackXYZ <rhythmupa@gmail.com>
2025-06-19 00:07:21 +0530

    clean number idk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+a cube timer <a class="underline text-blue-700" href="./app">use now</a>!
