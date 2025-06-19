ef9efa0b966c32fc18de988ee050e2a96da49203
RezHackXYZ <rhythmupa@gmail.com>
2025-06-19 16:16:34 -0700

    added a decent homepage!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,8 @@
 	let { children } = $props();
 </script>
 
-<div class="bg-white h-full">{@render children()}</div>
+<div class="bg-white h-full font-[Space_Grotesk]">{@render children()}</div>
+
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300&display=swap");
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-a cube timer <a class="underline text-blue-700" href="./app">use now</a>!
+<div class="flex h-full items-center justify-center">
+	<div class="flex h-full w-1/2 flex-col items-center justify-center gap-12 text-center">
+		<h1 class="text-6xl font-bold">An awesome cube timer [WIP] !</h1>
+
+		<div>
+			<p class="text-2xl">Heres the isuue: currently there are 2 widly used cubetimers:</p>
+			<ul class="list-disc text-left">
+				<li>
+					<a class="text-blue-700 underline" href="https://cstimer.net/">cstimer</a>: it has all the
+					features but looks ugly
+				</li>
+
+				<li>
+					<a class="text-blue-700 underline" href="https://www.cubedesk.io/">cubedesk</a>: a lot of
+					features are locked behind a pro subscription, and the open source repo doesnt include
+					them
+				</li>
+			</ul>
+		</div>
+
+		<div class="flex flex-col items-center gap-3">
+			<span class="text-3xl"
+				>so im fixing that by making a new cubetimer, which is open source and has all the features
+				and looks awesome!</span
+			>
+
+			<a href="./app"><button class="btn green">try now!</button></a>
+		</div>
+	</div>
+</div>
