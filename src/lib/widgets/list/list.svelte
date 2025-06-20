e3fccba7121fb4325daf0d4f05a424d72c3f809e
RezHackXYZ <rhythmupa@gmail.com>
2025-06-20 14:15:25 -0700

    make the list better

--- src/lib/widgets/list/list.svelte
+++ src/lib/widgets/list/list.svelte
@@ -2,19 +2,16 @@
 	import { ListOfSolves } from "$lib/store.svelte";
 </script>
 
-<div class="w-[270px] rounded border-2 border-black py-3">
-	<div class="flex max-h-[270px] flex-col gap-2 overflow-y-auto outline-2 outline-gray-200 mx-3 rounded">
+<div class="flex w-[270px] flex-col items-center gap-2 rounded border-2 border-black p-3">
+	<div
+		class="grid h-full max-h-[270px] w-full grid-cols-[min-content_min-content_auto] items-baseline gap-x-1 overflow-y-auto rounded px-4 outline-2 outline-gray-200"
+	>
 		{#each ListOfSolves.v as solve, index}
-			<div class="flex items-baseline">
-				<span class="mr-1 w-8 text-right font-mono text-xs text-gray-500">{index + 1}.</span>
-				<span>{solve}</span>
-				<span class="mr-5 ml-auto w-fit">
-					<i
-						class="nf nf-md-dots_vertical inline-block cursor-pointer leading-none text-gray-300 transition hover:scale-125 hover:text-black"
-					>
-					</i>
-				</span>
-			</div>
+			<span class="text-right font-mono text-xs text-gray-500">{index + 1}.</span>
+			<span>{solve}</span>
+			<span class="flex justify-end text-gray-300">
+				<i class="nf nf-md-dots_vertical cursor-pointer transition hover:text-black"></i>
+			</span>
 		{/each}
 	</div>
 </div>
